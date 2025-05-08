3b16505f2579cd96fe23e9cf308dfefe0ddf770c
tommelt <thomas.meltzer1@gmail.com>
2025-05-08 13:48:09 +0100

    chore: removed mdb commands from bench script

--- play/benchmark.sh
+++ play/benchmark.sh
@@ -30,19 +30,4 @@ file="mpigdb-asan-${NPROC}.out"
 mpigdb --mpigdb_dbg_arg -x --mpigdb_dbg_arg debug-scripts/mpigdb-script.sh -np $NPROC -- ./build-asan/heatdist > $file 2>&1
 echo "finished $file"
 
-mdb launch -n $NPROC -b gdb -t ./build-asan/heatdist > /dev/null 2>&1 &
-sleep ${nsecs}
-file="mdb-asan-${NPROC}.out"
-mdb attach -x debug-scripts/asan.mdb > $file 2>&1
-echo "finished $file"
-
-echo "sleeping ${nsecs}s..."
-sleep ${nsecs}
-
-mdb launch -n $NPROC -b vgdb -t ./build/heatdist > /dev/null 2>&1 &
-sleep ${nsecs}
-file="mdb-valgrind-${NPROC}.out"
-mdb attach -h 127.0.1.1 -p 2000 -x debug-scripts/valgrind.mdb > $file 2>&1
-echo "finished $file"
-
 grep "time:" *"-${NPROC}.out"
